18aeaadc762716f10262355449439ceaa3389d81
lhj244249 <115205492+hajin0221@users.noreply.github.com>
2022-11-09 21:51:25 +0900

    ok

---
+++ homework_mid_final/sketch.js
@@ -0,0 +1,105 @@
+// Angry Birds
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/138-angry-birds.html
+// https://youtu.be/TDQzoe9nslY
+
+const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
+
+let ground;
+const boxes = [];
+let bird;
+let world, engine;
+let mConstraint;
+let slingshot;
+
+let dotImg;
+let boxImg;
+let bkgImg;
+
+function preload() {
+  dotImg = loadImage('images/dot.png');
+  boxImg = loadImage('images/equals.png');
+  bkgImg = loadImage('images/skyBackground.png');
+}
+
+function setup() {
+  const canvas = createCanvas(711, 400);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width / 2, height - 10, width, 20);
+  for (let i = 0; i < 3; i++) {
+    boxes[i] = new Box(450, 300 - i * 75, 84, 100);
+  }
+  bird = new Bird(150, 300, 25);
+
+  slingshot = new SlingShot(150, 300, bird.body);
+
+  const mouse = Mouse.create(canvas.elt);
+  const options = {
+    mouse: mouse,
+  };
+
+  // A fix for HiDPI displays
+  mouse.pixelRatio = pixelDensity();
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    World.remove(world, bird.body);
+    bird = new Bird(150, 300, 25);
+    slingshot.attach(bird.body);
+  }
+}
+
+function mouseReleased() {
+  setTimeout(() => {
+    slingshot.fly();
+  }, 100);
+}
+
+function draw() {
+  background(bkgImg);
+  Matter.Engine.update(engine);
+  ground.show();
+  for (let box of boxes) {
+    box.show();
+  }
+  slingshot.show();
+  bird.show();
+}
+
+function setup() {
+  createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  //Engine.run(engine);
+  var options = {
+    isStatic: true,
+  };
+  ground = Bodies.rectangle(200, height, width, 100, options);
+
+  World.add(world, ground, bird, slingshot);
+}
+
+// function keyPressed() {
+//   if (key == ' ') {
+//   }
+// }
+
+function mousePressed() {
+  boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
+  noStroke(255);
+  fill(170);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width, 100);
+}
