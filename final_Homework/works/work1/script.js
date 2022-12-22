b673377abf50b6a950ffa070e0ab9a2339cefb86
lhj244249 <115205492+hajin0221@users.noreply.github.com>
2022-12-23 02:33:54 +0900

    ok

--- final_Homework/works/work1/script.js
+++ final_Homework/works/work1/script.js
@@ -6,216 +6,137 @@ function setup() {
   canvas.parent('p5Canvas');
 }
 
-var canvas = $('#wrapper-canvas').get(0);
+const { Engine, Render, Bodies, World, MouseConstraint, Composites, Query } =
+  Matter;
 
-var dimensions = {
-  width: $(window).width(),
-  height: $(window).height(),
+const sectionTag = document.querySelector('section.shapes');
+
+//width and height of the page
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+const engine = Engine.create();
+
+const renderer = Render.create({
+  element: sectionTag,
+  engine: engine,
+  options: {
+    width: w,
+    height: h,
+    background: '#000000',
+    wireframes: false,
+    pixelRatio: window.devicePixelRatio,
+  },
+});
+
+/*
+window.addEventListener("resize", function(){
+     w = window.innerWidth;
+     h = window.innerHeight;
+  });
+*/
+
+const createShape = function (x, y) {
+  return Bodies.circle(x, y, 25, {
+    frictionAir: 0.05,
+    render: {
+      sprite: {
+        texture: 'https://i.ibb.co/X4tm54x/smiley.png',
+        yScale: 0.5,
+        xScale: 0.5,
+      },
+    },
+  });
 };
 
-Matter.use('matter-attractors');
-Matter.use('matter-wrap');
+//Create a Static Element
+const bigBall = Bodies.circle(w / 2, h / 2, Math.min(w / 4, h / 4), {
+  isStatic: true,
+  render: {
+    fillStyle: 'white',
+    visible: true,
+  },
+});
 
-function runMatter() {
-  // module aliases
-  var Engine = Matter.Engine,
-    Events = Matter.Events,
-    Runner = Matter.Runner,
-    Render = Matter.Render,
-    World = Matter.World,
-    Body = Matter.Body,
-    Mouse = Matter.Mouse,
-    Common = Matter.Common,
-    Composite = Matter.Composite,
-    Composites = Matter.Composites,
-    Bodies = Matter.Bodies;
+// Create a wall for the shapes to bounce off
+const wallOptions = {
+  isStatic: true,
+  render: {
+    visible: true,
+  },
+};
 
-  // create engine
-  var engine = Engine.create();
+const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, wallOptions);
+const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 100, wallOptions);
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions);
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions);
 
-  engine.world.gravity.y = 0;
-  engine.world.gravity.x = 0;
-  engine.world.gravity.scale = 0.1;
-
-  // create renderer
-  var render = Render.create({
-    element: canvas,
-    engine: engine,
-    options: {
-      showVelocity: false,
-      width: dimensions.width,
-      height: dimensions.height,
-      wireframes: false,
-      background: 'rgb(240,240,240)',
+const mouseControl = MouseConstraint.create(engine, {
+  element: sectionTag,
+  constraint: {
+    render: {
+      visible: false,
     },
+  },
+});
+
+const intialShapes = Composites.stack(50, 50, 15, 5, 40, 40, function (x, y) {
+  return createShape(x, y);
+});
+
+World.add(engine.world, [
+  bigBall,
+  ground,
+  ceiling,
+  leftWall,
+  rightWall,
+  mouseControl,
+  intialShapes,
+]);
+
+document.addEventListener('click', function (event) {
+  const shape = createShape(event.pageX, event.pageY);
+  intialShapes.bodies.push(shape);
+  World.add(engine.world, shape);
+});
+
+//when we move our mouse check matter for collions
+//does the mouse touch the body
+document.addEventListener('mousemove', function (event) {
+  const vector = { x: event.pageX, y: event.pageY };
+  const hoveredShapes = Query.point(intialShapes.bodies, vector);
+
+  //console.log("hoveredShapes")
+
+  hoveredShapes.forEach((shape) => {
+    shape.render.sprite = null;
+    shape.render.fillStyle = 'red';
   });
+});
 
-  // create runner
-  var runner = Runner.create();
+Engine.run(engine);
+Render.run(renderer);
 
-  // Runner.run(runner, engine);
-  // Render.run(render);
+window.addEventListener('resize', function () {});
 
-  // create demo scene
-  var world = engine.world;
-  world.gravity.scale = 0;
+// Add Gravity
 
-  // create a body with an attractor
-  var attractiveBody = Bodies.circle(
-    render.options.width / 2,
-    render.options.height / 2,
-    Math.max(dimensions.width / 4, dimensions.height / 4) / 2,
-    {
-      render: {
-        fillStyle: `rgb(240,240,240)`,
-        strokeStyle: `rgb(240,240,240)`,
-        lineWidth: 0,
-      },
+let time = 0;
+const changeGravity = function () {
+  time = time + 0.01;
 
-      isStatic: true,
-      plugin: {
-        attractors: [
-          function (bodyA, bodyB) {
-            return {
-              x: (bodyA.position.x - bodyB.position.x) * 1e-6,
-              y: (bodyA.position.y - bodyB.position.y) * 1e-6,
-            };
-          },
-        ],
-      },
-    }
-  );
+  engine.world.gravity.x = Math.sin(time);
+  engine.world.gravity.y = Math.cos(time);
 
-  World.add(world, attractiveBody);
+  requestAnimationFrame(changeGravity);
+};
 
-  // add some bodies that to be attracted
-  for (var i = 0; i < 60; i += 1) {
-    let x = Common.random(0, render.options.width);
-    let y = Common.random(0, render.options.height);
-    let s =
-      Common.random() > 0.6 ? Common.random(10, 80) : Common.random(4, 60);
-    let poligonNumber = Common.random(3, 6);
-    var body = Bodies.polygon(
-      x,
-      y,
-      poligonNumber,
-      s,
+changeGravity();
 
-      {
-        mass: s / 20,
-        friction: 0,
-        frictionAir: 0.02,
-        angle: Math.round(Math.random() * 360),
-        render: {
-          fillStyle: '#FFFFFF',
-          strokeStyle: `#DDDDDD`,
-          lineWidth: 2,
-        },
-      }
-    );
-
-    World.add(world, body);
-
-    let r = Common.random(0, 1);
-    var circle = Bodies.circle(x, y, Common.random(2, 8), {
-      mass: 0.1,
-      friction: 0,
-      frictionAir: 0.01,
-      render: {
-        fillStyle: r > 0.3 ? `#FF2D6A` : `rgb(240,240,240)`,
-        strokeStyle: `#E9202E`,
-        lineWidth: 2,
-      },
-    });
-
-    World.add(world, circle);
-
-    var circle = Bodies.circle(x, y, Common.random(2, 20), {
-      mass: 6,
-      friction: 0,
-      frictionAir: 0,
-      render: {
-        fillStyle: r > 0.3 ? `#4267F8` : `rgb(240,240,240)`,
-        strokeStyle: `#3257E8`,
-        lineWidth: 4,
-      },
-    });
-
-    World.add(world, circle);
-
-    var circle = Bodies.circle(x, y, Common.random(2, 30), {
-      mass: 0.2,
-      friction: 0.6,
-      frictionAir: 0.8,
-      render: {
-        fillStyle: `rgb(240,240,240)`,
-        strokeStyle: `#FFFFFF`,
-        lineWidth: 3,
-      },
-    });
-
-    World.add(world, circle);
-  }
-
-  // add mouse control
-  var mouse = Mouse.create(render.canvas);
-
-  Events.on(engine, 'afterUpdate', function () {
-    if (!mouse.position.x) return;
-    // smoothly move the attractor body towards the mouse
-    Body.translate(attractiveBody, {
-      x: (mouse.position.x - attractiveBody.position.x) * 0.12,
-      y: (mouse.position.y - attractiveBody.position.y) * 0.12,
-    });
-  });
-
-  // return a context for MatterDemo to control
-  let data = {
-    engine: engine,
-    runner: runner,
-    render: render,
-    canvas: render.canvas,
-    stop: function () {
-      Matter.Render.stop(render);
-      Matter.Runner.stop(runner);
-    },
-    play: function () {
-      Matter.Runner.run(runner, engine);
-      Matter.Render.run(render);
-    },
-  };
-
-  Matter.Runner.run(runner, engine);
-  Matter.Render.run(render);
-  return data;
-}
-
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
-
-function setWindowSize() {
-  let dimensions = {};
-  dimensions.width = $(window).width();
-  dimensions.height = $(window).height();
-
-  m.render.canvas.width = $(window).width();
-  m.render.canvas.height = $(window).height();
-  return dimensions;
-}
-
-let m = runMatter();
-setWindowSize();
-$(window).resize(debounce(setWindowSize, 250));
+//Change gravity based on device orientation
+/*
+window.addEventListener("deviceorientation", function(event){
+  engine.world.gravity.x = event.gamma
+  engine.world.gravity.y = event.beta
+})
+*/
