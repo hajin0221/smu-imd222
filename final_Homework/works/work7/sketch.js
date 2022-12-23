d184372f4aa33d873dd893f9707dc0b2d22aeabc
lhj244249 <115205492+hajin0221@users.noreply.github.com>
2022-12-23 13:29:17 +0900

    ok

--- final_Homework/works/work7/sketch.js
+++ final_Homework/works/work7/sketch.js
@@ -1,101 +1,73 @@
-let sketchRNN;
-let currentStroke;
-let x, y;
-let nextPen = 'down';
-let seedPath = [];
-let seedPoints = [];
-let personDrawing = false;
-
-function preload() {
-  sketchRNN = ml5.sketchRNN('catpig');
-}
-
-function startDrawing() {
-  personDrawing = true;
-  x = mouseX;
-  y = mouseY;
-}
-
-function sketchRNNStart() {
-  personDrawing = false;
-
-  // Perform RDP Line Simplication
-  const rdpPoints = [];
-  const total = seedPoints.length;
-  const start = seedPoints[0];
-  const end = seedPoints[total - 1];
-  rdpPoints.push(start);
-  rdp(0, total - 1, seedPoints, rdpPoints);
-  rdpPoints.push(end);
-
-  // Drawing simplified path
-  background(255);
-  stroke(0);
-  strokeWeight(4);
-  beginShape();
-  noFill();
-  for (let v of rdpPoints) {
-    vertex(v.x, v.y);
-  }
-  endShape();
-
-  x = rdpPoints[rdpPoints.length - 1].x;
-  y = rdpPoints[rdpPoints.length - 1].y;
-
-  seedPath = [];
-  // Converting to SketchRNN states
-  for (let i = 1; i < rdpPoints.length; i++) {
-    let strokePath = {
-      dx: rdpPoints[i].x - rdpPoints[i - 1].x,
-      dy: rdpPoints[i].y - rdpPoints[i - 1].y,
-      pen: 'down',
-    };
-    seedPath.push(strokePath);
+// 이 클래스는 전체 구조와 사각형의 움직임을
+// 표현합니다.
+class Brick {
+  constructor(bc, y) {
+    this.brickColor = bc;
+    this.yPos = y;
+    this.xPos = 0;
   }
 
-  sketchRNN.generate(seedPath, gotStrokePath);
+  // 이 함수로 사각형 생성하기
+  createBrick() {
+    fill(this.brickColor);
+    rect(this.xPos, this.yPos, 100, 50);
+  }
+
+  // 이 함수로 사각형의 움직임을
+  // 1로 설정하기
+  setSpeed() {
+    this.xSpeed = 1;
+  }
+
+  // 이 함수로 사각형을 움직이기
+  moveBrick() {
+    this.xPos += this.xSpeed;
+    if (this.xPos + 100 >= width || this.xPos <= 0) {
+      this.xSpeed *= -1;
+    }
+  }
 }
 
 function setup() {
-  let canvas = createCanvas(400, 400);
-  canvas.mousePressed(startDrawing);
-  canvas.mouseReleased(sketchRNNStart);
-  background(255);
-  //sketchRNN.generate(gotStrokePath);
-  console.log('model loaded');
+  let boundingRects = document
+    .getElementById('p5Canvas')
+    .getBoundingClientRect();
+  let canvas = createCanvas(boundingRects.width, boundingRects.height);
+  canvas.parent('p5Canvas');
+  createP('Keep the mouse clicked').style('color', '#ffffff');
+  createP('to check whether the bricks').style('color', '#ffffff');
+  createP('are moving at same speed or not').style('color', '#ffffff');
 }
 
-function gotStrokePath(error, strokePath) {
-  //console.error(error);
-  //console.log(strokePath);
-  currentStroke = strokePath;
-}
+// 두 개의 사각형을 각각
+// 흰색과 검정색으로 지정
+let brick1 = new Brick('white', 100);
+let brick2 = new Brick('black', 250);
+
+//
+brick1.setSpeed();
+brick2.setSpeed();
 
 function draw() {
-  stroke(0);
-  strokeWeight(4);
-
-  if (personDrawing) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    seedPoints.push(createVector(mouseX, mouseY));
+  background(0);
+  if (mouseIsPressed) {
+    background(50);
   }
+  brick1.createBrick();
+  brick1.moveBrick();
+  if (!mouseIsPressed) {
+    createBars();
+  }
+  brick2.createBrick();
+  brick2.moveBrick();
+}
 
-  if (currentStroke) {
-    if (nextPen == 'end') {
-      sketchRNN.reset();
-      sketchRNNStart();
-      currentStroke = null;
-      nextPen = 'down';
-      return;
-    }
-
-    if (nextPen == 'down') {
-      line(x, y, x + currentStroke.dx, y + currentStroke.dy);
-    }
-    x += currentStroke.dx;
-    y += currentStroke.dy;
-    nextPen = currentStroke.pen;
-    currentStroke = null;
-    sketchRNN.generate(gotStrokePath);
+// 이 함수로 화면에
+// 검정색 및 흰색 막대기들을 생성하기
+function createBars() {
+  let len = 12;
+  for (let i = 0; i < width / len; i++) {
+    fill('white');
+    if (i % 2 == 0) rect(i * len, height, len, -height);
   }
 }
