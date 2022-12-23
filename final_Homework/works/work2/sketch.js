67b10c04fac08c226e5384404e48a2dea825147b
lhj244249 <115205492+hajin0221@users.noreply.github.com>
2022-12-23 13:58:30 +0900

    ok

--- final_Homework/works/work2/sketch.js
+++ final_Homework/works/work2/sketch.js
@@ -1,9 +1,14 @@
-var font;
-var vehicles = [];
+// Coding Challenge 128: SketchRNN Snowflakes
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/128-sketchrnn-snowflakes.html
+// https://youtu.be/pdaNttb7Mr8
+// https://editor.p5js.org/codingtrain/sketches/B1NHS00xE
 
-function preload() {
-  font = loadFont('AvenirNextLTPro-Demi.otf');
-}
+let model;
+let strokePath = null;
+
+let x, y;
+let pen;
 
 function setup() {
   let boundingRects = document
@@ -11,33 +16,57 @@ function setup() {
     .getBoundingClientRect();
   let canvas = createCanvas(boundingRects.width, boundingRects.height);
   canvas.parent('p5Canvas');
-  background(51);
-  // textFont(font);
-  // textSize(192);
-  // fill(255);
-  // noStroke();
-  // text('train', 100, 200);
+  setupNewSketch();
+  model = ml5.SketchRNN('snowflake', modelReady);
+  background(0);
+}
 
-  var points = font.textToPoints('hajin', 100, 200, 192, {
-    sampleFactor: 0.25,
-  });
-
-  for (var i = 0; i < points.length; i++) {
-    var pt = points[i];
-    var vehicle = new Vehicle(pt.x, pt.y);
-    vehicles.push(vehicle);
-    // stroke(255);
-    // strokeWeight(8);
-    // point(pt.x, pt.y);
-  }
+function modelReady() {
+  console.log('model ready');
+  model.reset();
+  model.generate(gotSketch);
 }
 
 function draw() {
-  background(51);
-  for (var i = 0; i < vehicles.length; i++) {
-    var v = vehicles[i];
-    v.behaviors();
-    v.update();
-    v.show();
+  translate(width / 2, height / 2);
+  if (strokePath != null) {
+    let newX = x + strokePath.dx * 0.2;
+    let newY = y + strokePath.dy * 0.2;
+    if (pen == 'down') {
+      stroke(255);
+      strokeWeight(2);
+      line(x, y, newX, newY);
+    }
+    pen = strokePath.pen;
+    strokePath = null;
+    x = newX;
+    y = newY;
+
+    if (pen !== 'end') {
+      model.generate(gotSketch);
+    } else {
+      console.log('drawing complete');
+      // In the video forgot to reset the pen to "down"
+      // along with the x,y position so here is a new
+      // function that takes care of both!
+      setupNewSketch();
+      model.reset();
+      model.generate(gotSketch);
+    }
   }
+}
+
+function gotSketch(error, s) {
+  if (error) {
+    console.error(error);
+  } else {
+    strokePath = s;
+    //console.log(strokePath);
+  }
+}
+
+function setupNewSketch() {
+  pen = 'down';
+  x = random(-width / 2, width / 2);
+  y = random(-height / 2, height / 2);
 }
